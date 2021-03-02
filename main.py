6b91cfe18598d79a1c1780d6ba699ceb89150235
Tyukina Arina <arina.tyukina@yandex.ru>
2021-03-02 14:41:06 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+class Field:
+    def __init__(self):
+        self.__value = None
+    # TODO: add your code here
+
+
+if __name__ == "__main__":
+    # Check if this code is working
+    field = Field()
+    field.set_value(123)
+    print(field.get_value())
