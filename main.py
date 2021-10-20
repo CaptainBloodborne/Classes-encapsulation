ccbacd29d5f5a110f9edb9930fa4d1fed152b1cb
artem <vrail30@outlook.com>
2021-10-20 19:44:44 +0300

    Task done.

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 class Field:
     def __init__(self):
         self.__value = None
-    # TODO: add your code here
+
+    def set_field(self, value):
+        self.__value = value
+
+    def get_field(self):
+        return self.__value
 
 
 if __name__ == "__main__":
     # Check if this code is working
     field = Field()
-    field.set_value(123)
-    print(field.get_value())
+    field.set_field(123)
+    print(field.get_field())
